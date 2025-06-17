044a05888bf6b5a3b221fb3fe9f7907064ba8118
Dev Ayush <143614582+Explicable11@users.noreply.github.com>
2025-06-17 15:44:08 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // DOM elements for sign in/Sign Up
+    // DOM elements for sign in/sign Up
     const container = document.querySelector('.container');
     const signUpBtn = document.querySelector('#sign-up-btn');
     const signInBtn = document.querySelector('#sign-in-btn');
