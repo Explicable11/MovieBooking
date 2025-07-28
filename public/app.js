9aff3dccb6d611d32d076425a513a3ed41937442
Dev Ayush <143614582+Explicable11@users.noreply.github.com>
2025-07-28 13:31:13 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const loginError = document.getElementById('loginError');
     const registerError = document.getElementById('registerError');
 
-    // DOM elements for Movies Page
+    // DOM elements for movies Page
     const moviesContainer = document.getElementById('moviesContainer');
     const moviesGrid = document.getElementById('moviesGrid');
     const logoutBtn = document.getElementById('logoutBtn');
